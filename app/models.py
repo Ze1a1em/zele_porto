93b9c74f047855edddba0aa6dbbf8e4443ff239a
ZS <zelalemsmith@Zelalems-MacBook-Air.local>
2022-09-28 21:18:15 +0300

    saved forms and add new styles.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=200)
@@ -9,4 +9,5 @@ class Contact(models.Model):
     def __str__(self):
         return self.message
 
-    
+    def get_absolute_url(self):
+        return reverse('thanks')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
+from django.urls import  reverse
 from .forms import ContactForm
 from .models import Contact
 # Create your views here.
@@ -8,9 +9,21 @@ class HomeView(CreateView):
     form_class = ContactForm
     template_name = 'index.html'
 
+    
+
     def submit_form(self, request):
-        if self.request:
-            print(form)
+
+        if self.request == "POST":
+            form = ContactForm(request.POST)
+
+            if form.is_valid():
+                form.save(commit=True)
+                return reverse('app:thanks')
+            else:
+                print("Erro")
+        
+        return render(request, 'index.html')
+
 
 
 class FormIsSubmitedView(TemplateView):
